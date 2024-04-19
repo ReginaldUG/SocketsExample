a763d89d936ff5319903232444fa48a0e79bdff4
ReginaldUG <reg4real26@gmail.com>
2024-04-19 23:14:47 +0100

    ServerThreads to allow multiple connections from clients concurrently and sending multiple data without the server closing connection

---
+++ Client2.py
@@ -0,0 +1,17 @@
+import socket
+
+#Specify the IP of the server, since its run on the same computer it will be the same ip as the computer
+HOST = '192.168.43.81'
+PORT = 9090     #port has to be the same as sevrer
+
+socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#since we are connecting we call the connect function
+socket.connect((HOST,PORT))
+
+stringvalue = str(45)
+socket.send(stringvalue.encode('utf-8'))
+print(socket.recv(1024).decode('utf-8'))
+
+
+#stringvalue = f"{str(id)}: {str(cx)} {str(cy)}"
+#print(stringvalue)
